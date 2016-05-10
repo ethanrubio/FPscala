3546da81783c8e51004afbcb84c57ad12a52c153
Ethan Rubio <ethanrubio@gmail.com>
2016-05-09 19:25:22 -0700

    refactors fib to use less conditionals

--- exercises/fibonacciTailRecursion.scala
+++ exercises/fibonacciTailRecursion.scala
@@ -1,12 +1,8 @@
 def fibonacciTailRecursion(n: Int): Int = {
 
-  def go(n: Int, previousNum: Int, acc: Int): Int = {
-    val newPreviousNum = acc
-    if (n <= 2) acc
-    else go(n - 1, newPreviousNum, previousNum + acc)
-  }
+  def go(n: Int, previousNum: Int, acc: Int): Int = 
+    if (n == 0) previousNum
+    else go(n - 1, acc, previousNum + acc)
 
-  if (n == 0) 0
-  else if (n <= 2) 1
-  else go(n, 1, 1)
+  go(n, 0, 1)
 }
