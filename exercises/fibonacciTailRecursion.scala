0943260c0fa32e4d14a8b02fe0bc6dbbaa8ec2fb
Ethan Rubio <ethanrubio@gmail.com>
2016-05-09 19:12:44 -0700

    adds solution to Fibonacci using tail recurison

---
+++ exercises/fibonacciTailRecursion.scala
@@ -0,0 +1,12 @@
+def fibonacciTailRecursion(n: Int): Int = {
+
+  def go(n: Int, previousNum: Int, acc: Int): Int = {
+    val newPreviousNum = acc;
+    if (n <= 2) acc
+    else go(n - 1, newPreviousNum, previousNum + acc)
+  }
+
+  if (n == 0) 0
+  else if (n <= 2) 1
+  else go(n, 1, 1)
+}
