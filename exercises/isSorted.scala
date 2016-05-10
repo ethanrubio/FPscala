f9525b00393437f9f1e0348932a70ff2a6940bf0
Ethan Rubio <ethanrubio@gmail.com>
2016-05-09 20:03:22 -0700

    adds isSorted solution

---
+++ exercises/isSorted.scala
@@ -0,0 +1,13 @@
+def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+  @annotation.tailrec
+  def loop(n: Int): Boolean = {
+    if (n >= as.length - 1) true
+    else if (!ordered(as(n), as(n + 1))) false
+    else loop(n + 1)
+  }
+
+  loop(0)
+}
+
+// test case
+// isSorted(Array(1, 4, 3), (a: Int, b: Int) => a < b)
