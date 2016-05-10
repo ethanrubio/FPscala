0bb8bfbf0712b265918e564cec5cda49ce8719f7
Ethan Rubio <ethanrubio@gmail.com>
2016-05-09 20:03:13 -0700

    adds annotation

--- exercises/fibonacciTailRecursion.scala
+++ exercises/fibonacciTailRecursion.scala
@@ -1,5 +1,5 @@
 def fibonacciTailRecursion(n: Int): Int = {
-
+  @annotation.tailrec
   def go(n: Int, previousNum: Int, acc: Int): Int = 
     if (n == 0) previousNum
     else go(n - 1, acc, previousNum + acc)
