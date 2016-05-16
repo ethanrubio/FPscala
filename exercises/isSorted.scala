cb1525943ed0c510663421bda920a3d0040493bc
Ethan Rubio <ethanrubio@gmail.com>
2016-05-15 19:54:26 -1000

    [bug] fixes ordered conditional

--- exercises/isSorted.scala
+++ exercises/isSorted.scala
@@ -2,7 +2,7 @@ def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
   @annotation.tailrec
   def loop(n: Int): Boolean = {
     if (n >= as.length - 1) true
-    else if (!ordered(as(n), as(n + 1))) false
+    else if (ordered(as(n), as(n + 1))) false
     else loop(n + 1)
   }
 
